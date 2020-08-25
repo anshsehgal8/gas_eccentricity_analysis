308805473f9ada62ba0392a405147bdc4908fa4f
Ansh Sehgal <anshsehgal8@gmail.com>
2020-08-24 23:12:33 -0700

    Update data flow with loader_tool.py

---
+++ loader_tool.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+
+saturation_orbits = [2200, 1800, 1500, 1200]
+alpha_keys  = ['a02', 'a04', 'a08', 'a16']
+
+
+def get_eccentricity_time_series_mm08(rundir, include_factor_of_two=True):
+    """
+    Gets the time series of: time, eccentricity magnitude, and the eccentricity
+    phase angle from a directory of HDF5 results
+    
+    :param      rundir:  The runid as a string
+    :type       rundir:  str
+    
+    :returns:   Three time series: the time, e's, and w's (the phases)
+    :rtype:     A tuple
+    """
+    filenames = sorted(glob.glob(os.path.join(rundir, 'eccentricity_measures.????.h5')))
+    t = np.array([h5py.File(f, 'r')['time']               [...] for f in filenames])
+    e = np.array([h5py.File(f, 'r')['mm08_e_global']      [...] for f in filenames]) * (2.0 if include_factor_of_two else 1.0)
+    w = np.array([h5py.File(f, 'r')['mm08_phase_global']  [...] for f in filenames])
+
+    return t, e, w
+
+
+
+
+def get_eccentricity_time_series_kd06(rundir):
+    """
+    Gets the time series of: time, eccentricity magnitude from a directory of
+    HDF5 results
+    
+    :param      rundir:  The runid as a string
+    :type       rundir:  str
+    
+    :returns:   Three time series: the time, e's, and w's (the phases)
+    :rtype:     A tuple
+    """
+    filenames = sorted(glob.glob(os.path.join(rundir, 'eccentricity_measures.????.h5')))
+    t = np.array([h5py.File(f, 'r')['time']             [...] for f in filenames])
+    e = np.array([h5py.File(f, 'r')['kd06_e_global']    [...] for f in filenames])
+    return t, e
+
+
+
+
+def get_disk_phase_time_series(rundir):
+    t, e, w = get_eccentricity_time_series_mm08(rundir)
+    dphi = np.diff(w)
+    dphi[dphi < -0.9 * 2 * np.pi] += 2 * np.pi
+    dphi[dphi > +0.9 * 2 * np.pi] -= 2 * np.pi
+    cumphase = np.cumsum(dphi)
+    return cumphase
+
+
+
+
+def calc_precession_versus_q(alpha_key, saturation_orbit):
+    def get_mean_phase(rundir):
+        runid = rundir.split('/')[-1]
+        q = int(runid[1:5])/10
+        cumphase = get_disk_phase_time_series(rundir)
+        t, e , w = get_eccentricity_time_series_mm08(rundir)
+        t = t[0:-1]
+        i = np.where(t / 2 / np.pi > saturation_orbit)
+        isat = int(saturation_orbit / 10)
+
+        if len(i[0]) > 10:
+            cumphase_mean = (cumphase[-1] - cumphase[isat]) / (t[-1] - t[isat])
+            cumphase_mean = (cumphase_mean)**(-1.)
+            return q, cumphase_mean
+        else:
+            print(f'warning: {runid} was not fully evolved')
+
+    rundirs = [f for f in sorted(glob.glob(f'data/q_suite_v2/q????-{alpha_key}-b64'))]
+    phases  = [x for x in [get_mean_phase(rundir) for rundir in rundirs] if x]
+    qs  = [e[0] for e in phases]
+    precessions = [e[1] for e in phases]
+    return qs, precessions
+
+
+
+
+def calc_eccentricity_versus_q(alpha_key, saturation_orbit):
+    def get_mean_es(rundir):
+        runid = rundir.split('/')[-1]
+        q = int(runid[1:5]) / 10
+        t1, e1, w  = get_eccentricity_time_series_mm08(rundir)
+        t2, e2     = get_eccentricity_time_series_kd06(rundir)
+        i1 = np.where(t1 / 2 / np.pi > saturation_orbit)
+        i2 = np.where(t2 / 2 / np.pi > saturation_orbit)
+        if len(i1[0]) > 10:
+            e1_mean = e1[i1].mean()
+            e2_mean = e2[i2].mean()
+            e1_min  = e1[i1].min()
+            e2_min  = e2[i2].min()
+            e1_max  = e1[i1].max()
+            e2_max  = e2[i2].max()
+            return q, e1_mean, e2_mean, e1_min, e2_min, e1_max, e2_max
+        else:
+            print(f'warning: {runid} was not fully evolved')
+            return None
+
+    rundirs = [f for f in sorted(glob.glob(f'data/q_suite_v2/q????-{alpha_key}-b64'))]
+    es      = [x for x in [get_mean_es(rundir) for rundir in rundirs] if x]
+    qs      = [e[0] for e in es]
+    e1s     = [e[1] for e in es]
+    e2s     = [e[2] for e in es]
+    e1mins  = [e[3] for e in es]
+    e2mins  = [e[4] for e in es]
+    e1maxs  = [e[5] for e in es]
+    e2maxs  = [e[6] for e in es]
+
+    return qs, e1s, e2s, e1mins, e2mins, e1maxs, e2maxs
+
+
+
+
+def write_totals_versus_q():
+
+    for alpha_key, saturation_orbit in zip(alpha_keys, saturation_orbits):
+        qs, e1s, e2s, e1mins, e2mins, e1maxs, e2maxs = calc_eccentricity_versus_q(alpha_key, saturation_orbit)
+        qs_prec, precessions = calc_precession_versus_q(alpha_key, saturation_orbit)
+
+        h5f = h5py.File(f'data_to_plot_{alpha_key}.h5', 'w')
+        h5f['qs']           = qs 
+        h5f['e1s']          = e1s
+        h5f['e2s']          = e2s
+        h5f['e1mins']       = e1mins
+        h5f['e2mins']       = e2mins
+        h5f['e1maxs']       = e1maxs
+        h5f['e2maxs']       = e2maxs
+        h5f['qs_prec']      = qs_prec
+        h5f['precessions']  = precessions
+
+
+
+
+def write_time_series_data():
+
+    rundirs = [f for f in sorted(glob.glob(f'data/q_suite_v2/q????-a??-b64'))]
+
+    for rundir in rundirs:
+        t_mm08, e_mm08, w = get_eccentricity_time_series_mm08(rundir)
+        t_kd06, e_kd06    = get_eccentricity_time_series_kd06(rundir)
+        cumphase          = get_disk_phase_time_series(rundir)
+        h5f = h5py.File(f"{rundir}/time_series.h5", 'w')
+        h5f['t_mm08']        = t_mm08
+        h5f['t_kd06']        = t_kd06
+        h5f['e_mm08']        = e_mm08
+        h5f['e_kd06']        = e_kd06
+        h5f['cumphase']      = cumphase
+
+
+    
+
+if __name__ == "__main__":
+
+    write_totals_versus_q()
+    write_time_series_data()
+
+ 
+
+
+
+
--- make_figures.py
+++ make_figures.py
@@ -5,152 +5,164 @@ import glob
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+import itertools
 
 
 
 
-def get_eccentricity_time_series_mm08(rundir, include_factor_of_two=True):
-    """
-    Gets the time series of: time, eccentricity magnitude, and the eccentricity
-    phase angle from a directory of HDF5 results
-    
-    :param      rundir:  The runid as a string
-    :type       rundir:  str
-    
-    :returns:   Three time series: the time, e's, and w's (the phases)
-    :rtype:     A tuple
-    """
-    filenames = sorted(glob.glob(os.path.join(rundir, 'eccentricity_measures.????.h5')))
-    t = np.array([h5py.File(f, 'r')['time']             [...] for f in filenames])
-    e = np.array([h5py.File(f, 'r')['mm08_e_global']    [...] for f in filenames]) * (2.0 if include_factor_of_two else 1.0)
-    w = np.array([h5py.File(f, 'r')['mm08_phase_global'][...] for f in filenames])
-    return t, e, w
+def plot_phase_time_series(ax,filename):	
+	t   = np.array(h5py.File(filename, 'r')['t_mm08'])
+	cumphase = h5py.File(filename, 'r')['cumphase']
+	orbits = t[0:-1] / 2 / np.pi
+	ax.plot(orbits,cumphase)
 
 
 
+def make_figure_phase_time_series():
+	fig = plt.figure()
+	ax1 = fig.add_subplot(1,1,1)
+	filenames = [f for f in sorted(glob.glob(f'data/q_suite_v2/q????-a08-b64/time_series.h5'))]
+	for filename in filenames:
+		plot_phase_time_series(ax1,filename)
+	plt.xlabel('Orbits')
+	plt.ylabel('Precession Rate')
+	return fig
 
-def get_eccentricity_time_series_kd06(rundir):
-    """
-    Gets the time series of: time, eccentricity magnitude from a directory of
-    HDF5 results
-    
-    :param      rundir:  The runid as a string
-    :type       rundir:  str
-    
-    :returns:   Three time series: the time, e's, and w's (the phases)
-    :rtype:     A tuple
-    """
-    filenames = sorted(glob.glob(os.path.join(rundir, 'eccentricity_measures.????.h5')))
-    t = np.array([h5py.File(f, 'r')['time']             [...] for f in filenames])
-    e = np.array([h5py.File(f, 'r')['kd06_e_global']    [...] for f in filenames])
-    return t, e
 
+
+def plot_eccentricity_time_series(ax1,ax2,filename):
+	orbits_1 = np.array(h5py.File(filename, 'r')['t_mm08'])
+	orbits_1 = orbits_1 / 2. / np.pi
+	e1       = h5py.File(filename, 'r')['e_mm08']
+	orbits_2 = np.array(h5py.File(filename, 'r')['t_kd06'])
+	orbits_2 = orbits_2 / 2. / np.pi
+	e2       = h5py.File(filename, 'r')['e_kd06']
+	ax1.plot(orbits_1, e1)
+	ax2.plot(orbits_2, e2)
 
 
 
 def make_figure_eccentricity_time_series():
-    rundirs = [f for f in sorted(glob.glob('data/q_suite_v1_reduced/q????-sc-b64'))]
-    colors = [plt.cm.viridis(n / len(rundirs)) for n in range(len(rundirs))]
+	fig = plt.figure()
+	ax1 = fig.add_subplot(1, 2, 1)
+	ax2 = fig.add_subplot(1, 2, 2)
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2)
+	filenames = [f for f in sorted(glob.glob(f'data/q_suite_v2/q????-a08-b64/time_series.h5'))]
 
-    for rundir, color in zip(rundirs, colors):
-        runid = rundir.split('/')[-1]
+	for filename in filenames:
+		plot_eccentricity_time_series(ax1, ax2, filename)
 
-        t, e, w = get_eccentricity_time_series_mm08(rundir)
-        ax1.plot(t / 2 / np.pi, e, label=runid, c=color)
-        ax1.text(t[-1] / 2 / np.pi, e[-1], runid)
+	ax1.set_yscale('log')
+	ax2.set_yscale('log')
+	ax1.set_ylim(1e-3, 1.0)
+	ax2.set_ylim(1e-3, 1.0)
+	ax1.set_xlabel('Orbits')
+	ax2.set_xlabel('Orbits')
+	ax1.set_title('MM08')
+	ax2.set_title('KD06')
+	ax1.set_ylabel('Eccentricity measure')
+	return fig
 
-        t, e = get_eccentricity_time_series_kd06(rundir)
-        ax2.plot(t / 2 / np.pi, e, label=runid, c=color)
-        ax2.text(t[-1] / 2 / np.pi, e[-1], runid)
 
-    ax1.set_yscale('log')
-    ax2.set_yscale('log')
-    ax1.set_ylim(1e-5, 1.0)
-    ax2.set_ylim(1e-5, 1.0)
-    ax1.set_xlabel('Orbits')
-    ax2.set_xlabel('Orbits')
-    ax1.set_title('MM08')
-    ax2.set_title('KD06')
-    ax1.set_ylabel('Eccentricity measure')
-    return fig
+
+def plot_precession_versus_q(ax, filename, marker):
+	qs = h5py.File(filename, 'r')['qs_prec']
+	precessions = h5py.File(filename, 'r')['precessions']
+	ax.plot(qs, precessions, marker=marker)
+
+
+
+def make_figure_precession_versus_q():
+	fig = plt.figure()
+	ax1 = fig.add_subplot(1,1,1)
+	filenames = ['data_to_plot_a02.h5', 'data_to_plot_a04.h5', 'data_to_plot_a08.h5', 'data_to_plot_a16.h5']
+	markers = ['+','^','.','*']
+	for filename, marker in zip(filenames, markers):
+		plot_precession_versus_q(ax1,filename, marker)
+	ax1.set_xlabel('Planet Mass (M_J)')
+	ax1.set_ylabel('Precession Rate (Orbits/Rev)')
+	#plt.axvline(x=50,color='r')
+	return fig
+
+
+
+def plot_eccentricity_versus_q(ax1, ax2, filename, marker):
+	qs      = h5py.File(filename, 'r')['qs']
+	e1s     = h5py.File(filename, 'r')['e1s']
+	e2s     = h5py.File(filename, 'r')['e2s']
+	e1mins  = h5py.File(filename, 'r')['e1mins']
+	e2mins  = h5py.File(filename, 'r')['e2mins']
+	e1maxs  = h5py.File(filename, 'r')['e1maxs']
+	e2maxs  = h5py.File(filename, 'r')['e2maxs'] 
+	ax1.plot(qs, e1s, marker = marker, markersize=8, color= 'black', linestyle='None',linewidth=0.5, mfc='None', label=filename[-6:-3])
+	ax2.plot(qs, e2s, marker = marker, markersize=8, color= 'black', linestyle='None',linewidth=0.5, mfc='None', label=filename[-6:-3])
+	ax1.fill_between(qs, e1mins, e1maxs, alpha=0.4)
+	ax2.fill_between(qs, e2mins, e2maxs, alpha=0.4)
+
 
 
 
 
 def make_figure_eccentricity_versus_q():
-    def get_mean_es(rundir):
-        runid = rundir.split('/')[-1]
-        q = int(runid[1:5]) / 10
-        t1, e1, w1 = get_eccentricity_time_series_mm08(rundir)
-        t2, e2     = get_eccentricity_time_series_kd06(rundir)
-        i1 = np.where(t1 / 2 / np.pi > 500)
-        i2 = np.where(t2 / 2 / np.pi > 500)
-        if len(i1[0]) > 30:
-            e1_mean = e1[i1].mean()
-            e2_mean = e2[i2].mean()
-            return q, e1_mean, e2_mean
-        else:
-            print(f'warning: {rundir} was not fully evolved')
-            return None
+	fig = plt.figure(figsize=[6.97,5.0])
+	ax1 = fig.add_subplot(1, 2, 1)
+	ax2 = fig.add_subplot(1, 2, 2)
+	#filenames = ['data_to_plot_sc.h5']
+	filenames = ['data_to_plot_a02.h5', 'data_to_plot_a04.h5', 'data_to_plot_a08.h5', 'data_to_plot_a16.h5']
+	markers = ['+','^','.','*']
+	#markers = ['.']
 
-    rundirs = [f for f in sorted(glob.glob('data/q_suite_v1_reduced/q????-sc-b64'))]
-    es  = [x for x in [get_mean_es(rundir) for rundir in rundirs] if x]
-    qs  = [e[0] for e in es]
-    e1s = [e[1] for e in es]
-    e2s = [e[2] for e in es]
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2)
-    ax1.plot(qs, e1s, '-o', mfc='none')
-    ax2.plot(qs, e2s, '-o', mfc='none')
-    ax1.set_xlabel(r'Mass ratio $[M_J]$')
-    ax2.set_xlabel(r'Mass ratio $[M_J]$')
-    ax1.set_ylabel('Eccentricity measure')
-    ax1.set_title('MM08')
-    ax2.set_title('KD06')
-    ax1.set_ylim(1e-5, 1.0)
-    ax2.set_ylim(1e-5, 1.0)
-    ax1.set_xscale('log')
-    ax2.set_xscale('log')
-    ax1.set_yscale('log')
-    ax2.set_yscale('log')
-    return fig
+	
+	for filename, marker in zip(filenames,markers):
+		plot_eccentricity_versus_q(ax1, ax2, filename, marker)
+	
+
+	ax1.set_xlabel(r'Planet Mass $[M_J]$')
+	ax2.set_xlabel(r'Planet Mass $[M_J]$')
+	ax1.set_ylabel('Eccentricity measure')
+	ax1.set_title('MM08')
+	ax2.set_title('KD06')
+	ax1.set_ylim(1e-3, 1.0)
+	ax2.set_ylim(1e-3, 1.0)
+	ax1.set_xscale('log')
+	ax2.set_xscale('log')
+	ax1.set_yscale('log')
+	ax2.set_yscale('log')
+	ax1.legend()
+	ax2.legend()
+	return fig
 
 
 
 
 def configure_matplotlib(hardcopy=False):
-    plt.rc('xtick', labelsize=8)
-    plt.rc('ytick', labelsize=8)
-    plt.rc('axes', labelsize=8)
-    plt.rc('legend', fontsize=8)
-    plt.rc('font', family='Times New Roman' if hardcopy else 'DejaVu Sans', size=8)
-    plt.rc('text', usetex=hardcopy)
+	plt.rc('xtick', labelsize=8)
+	plt.rc('ytick', labelsize=8)
+	plt.rc('axes', labelsize=8)
+	plt.rc('legend', fontsize=8)
+	plt.rc('font', family='Times New Roman' if hardcopy else 'DejaVu Sans', size=8)
+	plt.rc('text', usetex=hardcopy)
 
 
 
 
 if __name__ == "__main__":
-    from argparse import ArgumentParser, RawTextHelpFormatter
-    figure_funcs = dict([(k[12:], v)  for k, v in locals().items() if k.startswith('make_figure')])
-    parser = ArgumentParser(formatter_class=RawTextHelpFormatter, epilog='\n'.join(['all'] + list(figure_funcs.keys())))
-    parser.add_argument('figures', nargs='+', metavar='figure', choices=['all'] + list(figure_funcs.keys()))
-    parser.add_argument('--hardcopy', action='store_true')
-    parser.add_argument('--open', action='store_true')
-    args = parser.parse_args()
-    configure_matplotlib(args.hardcopy)
-    figure_list = figure_funcs.keys() if args.figures == ['all'] else args.figures
-    for figure in figure_list:
-        fig = figure_funcs[figure]()
-        if args.hardcopy:
-            pdf_name = f'figures/{figure}.pdf'
-            print(f'saving {pdf_name}')
-            fig.savefig(f'{pdf_name}')
-            if args.open:
-                os.system(f'open {pdf_name}')
-    if not args.hardcopy:
-        plt.show()
+	from argparse import ArgumentParser, RawTextHelpFormatter
+	figure_funcs = dict([(k[12:], v)  for k, v in locals().items() if k.startswith('make_figure')])
+	parser = ArgumentParser(formatter_class=RawTextHelpFormatter, epilog='\n'.join(['all'] + list(figure_funcs.keys())))
+	parser.add_argument('figures', nargs='+', metavar='figure', choices=['all'] + list(figure_funcs.keys()))
+	parser.add_argument('--hardcopy', action='store_true')
+	parser.add_argument('--open', action='store_true')
+	args = parser.parse_args()
+	configure_matplotlib(args.hardcopy)
+	figure_list = figure_funcs.keys() if args.figures == ['all'] else args.figures
+	for figure in figure_list:
+		fig = figure_funcs[figure]()
+		if args.hardcopy:
+			pdf_name = f'figures/{figure}.pdf'
+			print(f'saving {pdf_name}')
+			fig.savefig(f'{pdf_name}')
+			if args.open:
+				os.system(f'open {pdf_name}')
+	if not args.hardcopy:
+		plt.show()
