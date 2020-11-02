35e49312f7fdebcf464e75c3b709108524c28e74
Ansh Sehgal <asehgal@Anshs-iMac.local>
2020-11-02 16:17:50 -0500

    Most recent penaltyattempt

--- plot_contour.py
+++ plot_contour.py
@@ -6,7 +6,7 @@ import scipy.interpolate
 import scipy.optimize
 import matplotlib.pyplot as plt
 import loaders
-#import cv2
+
 filename = 'data/q_suite_v2_diagnostics/q0600-a08-b64.diagnostics.0178.h5'
 
 def x_theta(xcm,ycm,a,b,omega):
@@ -23,14 +23,14 @@ def load_data():
 	domain_radius = np.array(h5f['run_config']['domain_radius'])
 	block_size    = np.array(h5f['run_config']['block_size'])
 	depth         = np.array(h5f['run_config']['depth'])
-	real_depth = depth - 4
+	real_depth = depth - 3
 	step  = (2 * domain_radius)/(2**real_depth * block_size)
 	vertices = h5f['vertices'].values()
 	helpers = loaders.HelperFunctions()
 	xc = np.array([helpers.cell_center_x_array(g[...]) for g in vertices])
 	yc = np.array([helpers.cell_center_y_array(g[...]) for g in vertices])
 	mass = loaders.get_dataset(filename, 'mass')
-	nbins = 16
+	nbins = 32
 	bins = np.linspace(-nbins * step ,nbins * step,(2 * nbins) + 1)
 
 	masses, xedges, yedges = np.histogram2d(xc.flatten(),yc.flatten(),weights=mass.flatten(),bins=bins)
@@ -53,7 +53,9 @@ def calc_sigma():
 		print(a,b)
 		#counts, bin_number = np.histogram(sigma,bins=100)
 		#plt.step(bin_number[:-1],counts)
-		return np.log(sigma).std() / np.log(sigma).mean() #+ abs(a - 2.5) + abs(b - 2.5) #+ abs(xcm + 0.2) + abs(ycm + 0.6)
+		#print(np.log10(np.mean((sigma - sigma.max())**2)))
+		return np.log10(np.mean((sigma - sigma.max())**2)) #+ abs(a - 2.5) + abs(b - 2.5)
+		#return np.log(sigma).std() / np.log(sigma).mean() #+ abs(a - 2.5) + abs(b - 2.5) #+ abs(xcm + 0.2) + abs(ycm + 0.6)
 
 	return lambda x: result(*x)
 
@@ -65,7 +67,7 @@ if __name__ == '__main__':
 	# parser = argparse.ArgumentParser()
 	# parser.add_argument("filename")
 	# args = parser.parse_args()
-	x0 = [0.,0.,1.,1.,0.]
+	x0 = [0.,0.,2.2,2.2,0.]
 	#print(calc_sigma(*x0))
 	residual  = scipy.optimize.minimize(calc_sigma(),x0)
 	masses, xedges, yedges = load_data()
@@ -81,23 +83,3 @@ if __name__ == '__main__':
 	print(residual.x)
 	print(e)
 	plt.show()
-
-
-# image = cv2.imread('figures/q0600-a08.png')
-# window_name = 'Image'
-# cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
-# center_coordinates = (1045,961)
-# axesLength = (419,385)
-# a = min(axesLength[0],axesLength[1])
-# b = max(axesLength[0],axesLength[1])
-# e = np.sqrt(1. - (a / b)**2 )
-# print(e)
-# angle = 0
-# startAngle = 0
-# endAngle = 360
-# color = (0,0,255)
-# thickness = 5
-
-# image = cv2.ellipse(image,center_coordinates,axesLength,angle,startAngle,endAngle,color,thickness)
-# cv2.imshow(window_name,image)
-# cv2.waitKey()
